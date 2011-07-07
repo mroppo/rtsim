254d47552e4d667a96de90b0d02d5057b4ccff3e
karina.ordunio@gmail.com <karina.ordunio@gmail.com@4364b6bc-ba75-35c4-c8b7-f0fbe107a164>
2011-07-07 21:18:45 +0000

    agregados comentarios

--- trunk/sched/rm/rm_rbound_mp/base_rm_global_sched.c
+++ trunk/sched/rm/rm_rbound_mp/base_rm_global_sched.c
@@ -4,12 +4,11 @@
 // 0. agregar el include donde se encuentra la funcion parcial
 // ################################################################3
 #include "rbound_mp.h"
-
-
-//name used for command call from TCL
 // ################################################################3
 // 1. nombre del comando que se usaran dentro de TCL para llamar al codigo nativo
 // ################################################################3
+//name used for command call from TCL
+//nombre del comando que se usaran dentro de TCL para llamar al codigo nativo
 #define COMMAND_NAME "rmrboundmp"
 //ejemplo de llamada desde TCL
 //set r [catch {eval COMMAND_NAME $algorithmSelected $numberProcessors $simulationTime $pathSavedTasks } errmsg]
@@ -27,9 +26,12 @@ processor_t* partial_function(processor_t* list, int nproc, char *file)
 	return list;
 }
 
+// Nombre del metodo comun usado para entrar a ejecutar el planificador
 static int SimuladorCmd(ClientData clientData, Tcl_CmdDeleteProc* proc, int objc, Tcl_Obj* const objv[]);
 
-//USAR NOMBRE DE LA LIBRERIA MAS _Init
+// ################################################################3
+// 4. Renombrar la fucion usando el nombre de la libreria + _Init
+// ################################################################3
 // ejemplo Rmnfll_init para librmnfll
 int Rmrboundmp_Init(Tcl_Interp *interp)
 {
@@ -41,6 +43,8 @@ int Rmrboundmp_Init(Tcl_Interp *interp)
     }
 	
 	DBG("\nCreating command [%s]\n",COMMAND_NAME);
+	
+	//al recibir de TCL el commando COMMAND_NAME llamar a la funcion SimuladorCmd
     Tcl_CreateObjCommand(interp, COMMAND_NAME, SimuladorCmd, NULL, NULL);
     Tcl_PkgProvide(interp, COMMAND_NAME, "1.1");
 
