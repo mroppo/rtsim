0788bb67135d5da694dc5b79f9776dc30f63145b
karina.ordunio <karina.ordunio@4364b6bc-ba75-35c4-c8b7-f0fbe107a164>
2011-09-26 00:22:21 +0000

    arreglado problema con multiples procesadores para crear archivo de salida.

---
+++ trunk/sched/edf/makeAll.sh
@@ -0,0 +1,7 @@
+
+make edf_global
+make edf_bf
+make edf_ff
+make edf_nf
+make edf_wf
+ 
---
+++ trunk/sched/rm/makeAll.sh
@@ -0,0 +1,18 @@
+
+make rm_global
+make rm_bf_du_uo
+make rm_bf_ip
+make rm_bf_ll
+make rm_exactAudsley
+make rm_exactLehoczky
+make rm_ff_du_uo
+make rm_ff_ip
+make rm_ff_ll
+make rm_gt
+make rm_nf_ip
+make rm_nf_ll
+make rm_rbound_mp
+make rm_rbound_mp_bf
+make rm_rbound_mp_nfr
+make rm_st
+make rm_wf_ll
